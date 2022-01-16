adf33c7f0e7662e6565641cf121b5d221935e7ef
meenoumeh <67861041+meenoumeh@users.noreply.github.com>
2022-01-16 19:53:58 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            IPAddress ip = IPAddress.Parse("192.168.1.70");
+            int port = 3000;
+            Server server = new Server(ip, port);
+
+            server.StartListenning();
+            Console.WriteLine("server stareted successfully ...");
+
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("waiting for connection ...");
+
+            server.AcceptClient();
+            Console.WriteLine("client connected ....");
+
+
+            string messageFromClient = "";
+            string messageToClient = "";
+
+
+            try
+            {
+                server.ClientData();
+                while (server.Status)
+                {
+
+                    if (server.SocketForClient.Connected)
+                    {
+                        messageFromClient = server.StreamReader.ReadLine();
+                        Console.WriteLine("client : " + messageFromClient);
+                        if (messageFromClient == "exit")
+                        {
+                            server.Status = false;
+                            server.StreamReader.Close();
+                            server.StreamWriter.Close();
+                            server.NetworkStream.Close();
+                            return;
+                        }
+
+                        Console.WriteLine("server : ");
+                        messageToClient = Console.ReadLine();
+                        server.StreamWriter.WriteLine(messageToClient);
+                        server.StreamWriter.Flush();
+                    }
+                }
+                server.Disconect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    public class Server
+    {
+        public IPAddress MyIP { get; private set; }
+        public int Port { get; private set; }
+        public bool Status = true;
+        public TcpListener TcpListener { get; set; }
+        public Socket SocketForClient { get; set; }
+
+        public NetworkStream NetworkStream { get; set; }
+        public StreamReader StreamReader { get; set; }
+        public StreamWriter StreamWriter { get; set; }
+
+        public Server(IPAddress iPAddress, int port)
+        {
+            MyIP = iPAddress;
+            Port = port;
+        }
+
+        public void StartListenning()
+        {
+            try
+            {
+                TcpListener = new TcpListener(MyIP, Port);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("can not start ....");
+            }
+        }
+
+        public void AcceptClient()
+        {
+            try
+            {
+                SocketForClient = TcpListener.AcceptSocket();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("can not accept client ...");
+            }
+        }
+
+        public void ClientData()
+        {
+            NetworkStream = new NetworkStream(SocketForClient);
+            StreamReader = new StreamReader(NetworkStream);
+            StreamWriter = new StreamWriter(NetworkStream);
+
+        }
+
+        public void Disconect()
+        {
+            NetworkStream.Close();
+            StreamWriter.Close();
+            StreamReader.Close();
+            SocketForClient.Close();
+        }
+
+    }
+}
